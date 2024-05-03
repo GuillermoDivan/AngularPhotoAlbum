4502d31a6441697e4f8e126e3ff09a9e8814e552
GuillermoDivan <guillermodivan@hotmail.com>
2024-05-03 05:01:08 -0300

    Tutorial version done

---
+++ src/app/pages/gallery/photo/photo.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Photo } from '../../../models/photo';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-photo',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './photo.component.html',
+  styleUrl: './photo.component.scss'
+})
+export class PhotoComponent {
+@Input()
+photo!:Photo
+}
